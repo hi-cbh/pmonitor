74c831498b1b6b5eb111c7a2b15ae601a3729842
apple <1>
2018-07-02 15:26:46 +0800

    调试：添加运行ip地址2

--- src/monitor/settings.py
+++ src/monitor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9td+rjgm+2h3*jl!)-wct5&dzp(0o=n-92%fy&cl#duq$f)nt%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.10.12']
+ALLOWED_HOSTS = ['localhost','192.168.10.12']
 
 
 # Application definition
